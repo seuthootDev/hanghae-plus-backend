5a14e7e5479b708e71811e9ddea63129353c2b3c
seuthootDev <seuthootdev@gmail.com>
2025-07-23 03:13:06 +0900

    feat: 앱 모듈 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,11 @@ import { ProductsController } from './presentation/controllers/products.controll
 import { OrdersController } from './presentation/controllers/orders.controller';
 import { CouponsController } from './presentation/controllers/coupons.controller';
 import { PaymentsController } from './presentation/controllers/payments.controller';
-import { UsersService } from './infrastructure/services/real/users.service';
-import { ProductsService } from './infrastructure/services/real/products.service';
-import { OrdersService } from './infrastructure/services/real/orders.service';
-import { CouponsService } from './infrastructure/services/real/coupons.service';
-import { PaymentsService } from './infrastructure/services/real/payments.service';
+import { UsersService } from './infrastructure/services/users.service';
+import { ProductsService } from './infrastructure/services/products.service';
+import { OrdersService } from './infrastructure/services/orders.service';
+import { CouponsService } from './infrastructure/services/coupons.service';
+import { PaymentsService } from './infrastructure/services/payments.service';
 import { USERS_SERVICE } from './application/interfaces/services/users-service.interface';
 import { PRODUCTS_SERVICE } from './application/interfaces/services/products-service.interface';
 import { ORDERS_SERVICE } from './application/interfaces/services/orders-service.interface';
@@ -23,6 +23,16 @@ import { CreateOrderUseCase } from './application/use-cases/orders/create-order.
 import { IssueCouponUseCase } from './application/use-cases/coupons/issue-coupon.use-case';
 import { GetUserCouponsUseCase } from './application/use-cases/coupons/get-user-coupons.use-case';
 import { ProcessPaymentUseCase } from './application/use-cases/payments/process-payment.use-case';
+import { USER_REPOSITORY } from './application/interfaces/repositories/user-repository.interface';
+import { PRODUCT_REPOSITORY } from './application/interfaces/repositories/product-repository.interface';
+import { ORDER_REPOSITORY } from './application/interfaces/repositories/order-repository.interface';
+import { COUPON_REPOSITORY } from './application/interfaces/repositories/coupon-repository.interface';
+import { PAYMENT_REPOSITORY } from './application/interfaces/repositories/payment-repository.interface';
+import { UserRepository } from './infrastructure/repositories/user.repository';
+import { ProductRepository } from './infrastructure/repositories/product.repository';
+import { OrderRepository } from './infrastructure/repositories/order.repository';
+import { CouponRepository } from './infrastructure/repositories/coupon.repository';
+import { PaymentRepository } from './infrastructure/repositories/payment.repository';
 
 @Module({
   imports: [],
@@ -64,6 +74,28 @@ import { ProcessPaymentUseCase } from './application/use-cases/payments/process-
     {
       provide: PAYMENTS_SERVICE,
       useClass: PaymentsService,
+    },
+    
+    // 리포지토리 인터페이스와 Mock 구현체 연결
+    {
+      provide: USER_REPOSITORY,
+      useClass: UserRepository,
+    },
+    {
+      provide: PRODUCT_REPOSITORY,
+      useClass: ProductRepository,
+    },
+    {
+      provide: ORDER_REPOSITORY,
+      useClass: OrderRepository,
+    },
+    {
+      provide: COUPON_REPOSITORY,
+      useClass: CouponRepository,
+    },
+    {
+      provide: PAYMENT_REPOSITORY,
+      useClass: PaymentRepository,
     },
   ],
 })
