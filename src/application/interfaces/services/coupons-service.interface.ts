39f8faf1f4eaada2655d9b5b0d539b77ba65a091
seuthootDev <seuthootdev@gmail.com>
2025-07-23 21:28:38 +0900

    feat: 서비스 인터페이스에서 응답 디티오를 사용하지 않음

--- src/application/interfaces/services/coupons-service.interface.ts
+++ src/application/interfaces/services/coupons-service.interface.ts
@@ -1,9 +1,10 @@
 import { IssueCouponDto } from '../../../presentation/dto/couponsDTO/issue-coupon.dto';
 import { CouponResponseDto } from '../../../presentation/dto/couponsDTO/coupon-response.dto';
+import { Coupon } from '../../../domain/entities/coupon.entity';
 
 export const COUPONS_SERVICE = 'COUPONS_SERVICE';
 
 export interface CouponsServiceInterface {
-  issueCoupon(issueCouponDto: IssueCouponDto): Promise<CouponResponseDto>;
-  getUserCoupons(userId: number): Promise<CouponResponseDto[]>;
+  issueCoupon(issueCouponDto: IssueCouponDto): Promise<Coupon>;
+  getUserCoupons(userId: number): Promise<Coupon[]>;
 } 
--- src/application/interfaces/services/payments-service.interface.ts
+++ src/application/interfaces/services/payments-service.interface.ts
@@ -1,8 +1,9 @@
 import { ProcessPaymentDto } from '../../../presentation/dto/paymentsDTO/process-payment.dto';
 import { PaymentResponseDto } from '../../../presentation/dto/paymentsDTO/payment-response.dto';
+import { Payment } from '../../../domain/entities/payment.entity';
 
 export const PAYMENTS_SERVICE = 'PAYMENTS_SERVICE';
 
 export interface PaymentsServiceInterface {
-  processPayment(processPaymentDto: ProcessPaymentDto): Promise<PaymentResponseDto>;
+  processPayment(processPaymentDto: ProcessPaymentDto): Promise<Payment>;
 } 
