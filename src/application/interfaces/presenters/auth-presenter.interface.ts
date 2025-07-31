e33532d5c8cd9952a432f3d703b175b89a2e928f
seuthootDev <seuthootdev@gmail.com>
2025-07-31 21:25:34 +0900

    delete: 프렌젠터 삭제

--- src/application/interfaces/presenters/auth-presenter.interface.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '../../../domain/entities/user.entity';
-import { AuthResponseDto } from '../../../presentation/dto/authDTO/auth-response.dto';
-
-export const AUTH_PRESENTER = 'AUTH_PRESENTER';
-
-export interface AuthPresenterInterface {
-  presentAuth(user: User, token: string, refreshToken: string): AuthResponseDto;
-} 
--- src/application/interfaces/presenters/order-presenter.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Order } from '../../../domain/entities/order.entity';
-import { OrderResponseDto } from '../../../presentation/dto/ordersDTO/order-response.dto';
-
-export const ORDER_PRESENTER = 'ORDER_PRESENTER';
-
-export interface OrderPresenterInterface {
-  presentOrder(order: Order): OrderResponseDto;
-  presentOrderList(orders: Order[]): OrderResponseDto[];
-} 
